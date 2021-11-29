e9386747806b8fa86c3776efdb9a7fef37e9494b
Chirayu Chaudhari <chirayuchaudhari1990@gmail.com>
2021-11-28 19:47:21 -0500

    New README

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -19,16 +19,16 @@ summary(lm(mpg ~ vehicle_length + ground_clearance, data=mecha_mpg))
 
 mecha_coil <- read.csv(file='./Resources/Suspension_Coil.csv',check.names=F,stringsAsFactors = F) 
 
-total_summary <- mecha_coil %>% summarize(Mean_PSI=mean(PSI),
-                                          Median_PSI=median(PSI),
-                                          Var_PSI=var(PSI),
-                                          Std_Dev_PSI=sd(PSI),
+total_summary <- mecha_coil %>% summarize(Mean=mean(PSI),
+                                          Median=median(PSI),
+                                          Var=var(PSI),
+                                          Std_Dev=sd(PSI),
                                           Num_Coil=n(), .groups = 'keep') 
                                                           
-lot_summary <- mecha_coil  %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI),
-                                                                         Median_PSI=median(PSI),
-                                                                         Var_PSI=var(PSI),
-                                                                         Std_Dev_PSI=sd(PSI),
+lot_summary <- mecha_coil  %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),
+                                                                         Median=median(PSI),
+                                                                         Var=var(PSI),
+                                                                         Std_Dev=sd(PSI),
                                                                          Num_Coil=n(), .groups = 'keep')                                      
 
 #PSI Whole lot
--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # MechaCar_Statistical_Analysis
+
+## Linear Regression to Predict MPG
+
+#### Which variables/coefficients provided a non-random amount of variance to the mpg values in the dataset?
+Vehicle Length and Vehicle Ground Clearance
+
+#### Is the slope of the linear model considered to be zero? Why or why not?
+The slope of the model is not zero because the value p-value is 5.35e-11 which is very small than the significance level of 0.05%.
+
+#### Does this linear model predict mpg of MechaCar prototypes effectively? Why or why not?
+The linear model predicts (r-squared value = 71%) of the mpg predictions. 
+
+![](https://github.com/c3crocks/MechaCar_Statistical_Analysis/blob/main/Images/Part1.JPG)
+
+
+
+## Summary Statistics on Suspension Coils
+
+#### Total Summary
+
+![](https://github.com/c3crocks/MechaCar_Statistical_Analysis/blob/main/Images/Part2-Total_Summary.JPG)
+
+#### Lot Summary
+
+![](https://github.com/c3crocks/MechaCar_Statistical_Analysis/blob/main/Images/Part2-lot_summary.JPG)
+
+
+
+#### The design specifications for the MechaCar suspension coils dictate that the variance of the suspension coils must not exceed 100 pounds per square inch. Does the current manufacturing data meet this design specification for all manufacturing lots in total and each lot individually? Why or why not?
+
+The entire population data variance is 62.29 PSI which is within 100 PSI as per design specifications. 
+However, when analyzing seperately lot 3 falls outside of the specification because the variance is 170.29 PSI. Lot 1 and Lot 2 are within limits.
